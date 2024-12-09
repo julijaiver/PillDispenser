835033f8335d76e5731d65181f77f44ac8be55be
julijai <julija.ivaskeviciute@metropolia.fi>
2024-12-09 12:54:33 +0000

    Update lora_connect.c

--- lora_connect.c
+++ lora_connect.c
@@ -35,10 +35,10 @@ void send_to_uart(uart_inst_t *uart, char *string) {
     uart_write_blocking(uart, string, strlen((char *)string));
 }
 
-bool send_command_to_lora(uint8_t *send, char *response, const char *command) {
+bool send_command_to_lora(uint8_t *send, char *response, const char *command, uint32_t timeout) {
     strncpy((char *)send, command, INPUT_SIZE);
     send_to_uart(uart1, send);
-    if (read_string_from_uart(uart1, 500000, response)) {
+    if (read_string_from_uart(uart1, timeout, response)) {
         printf("Mode response: %s\n", response);
         return true;
     } else {
