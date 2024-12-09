93561445a11c1007c58ebe8a8e511a1dd9564427
julijai <julija.ivaskeviciute@metropolia.fi>
2024-12-09 12:55:39 +0000

    Update lora_connect.h

--- lora_connect.h
+++ lora_connect.h
@@ -7,6 +7,7 @@
 #include "hardware/uart.h"
 
 void send_to_uart(uart_inst_t *uart, char *string);
+bool send_command_to_lora(uint8_t *send, char *response, const char *command, uint32_t timeout);
 bool read_string_from_uart(uart_inst_t *uart, uint32_t time_us, char *str);
 
 
